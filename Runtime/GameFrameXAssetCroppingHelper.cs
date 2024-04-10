7f34575b41c4902ae8a3b39f1bfb3b9062ef765c
AlianBlank <wangfj11@foxmail.com>
2024-04-10 18:03:34 +0800

    [增加]1. 增加防裁剪标记

--- Runtime/GameFrameXAssetCroppingHelper.cs
+++ Runtime/GameFrameXAssetCroppingHelper.cs
@@ -1,16 +1,18 @@
 using UnityEngine;
+using UnityEngine.Scripting;
 
 namespace GameFrameX.Asset.Runtime
 {
+    [Preserve]
     public class GameFrameXAssetCroppingHelper : MonoBehaviour
     {
+        [Preserve]
         private void Start()
         {
             _ = typeof(AssetManager);
             _ = typeof(Constant);
             _ = typeof(IAssetManager);
             _ = typeof(AssetComponent);
-
         }
     }
 }
