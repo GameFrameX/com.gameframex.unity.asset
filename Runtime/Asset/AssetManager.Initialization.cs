401c73cc7819222c38d54cc949ec50e561b3dcad
Blank <wangfj11@foxmail.com>
2026-03-06 18:46:36 +0800

    refactor(Asset): 重构YooAsset初始化方法以提升可读性和维护性
    - 提取默认包名常量，避免硬编码
    - 为所有初始化方法添加XML文档注释
    - 简化编辑器模拟模式初始化逻辑
    - 重构WebGL模式初始化，统一文件系统创建逻辑
    - 使用对象初始化器简化主机模式参数创建
    - 移除未使用的常量和注释代码

--- Runtime/Asset/AssetManager.Initialization.cs
+++ Runtime/Asset/AssetManager.Initialization.cs
@@ -8,6 +8,8 @@ namespace GameFrameX.Asset.Runtime
 {
     public partial class AssetManager
     {
+        public const string ConstDefaultPackageName = "DefaultPackage";
+        
         /// <summary>
         /// 根据运行模式创建初始化操作数据
         /// </summary>
@@ -44,19 +46,25 @@ namespace GameFrameX.Asset.Runtime
             }
         }
 
-        private const string ASSET_BUNDLE_PACKAGE_ROOT_KEY = "T1_ASSET_BUNDLE_PACKAGE_ROOT_KEY";
-
+        /// <summary>
+        /// 初始化YooAsset编辑器模拟运行模式
+        /// </summary>
+        /// <param name="resourcePackage">资源包</param>
+        /// <returns></returns>
         [UnityEngine.Scripting.Preserve]
         private InitializationOperation InitializeYooAssetEditorSimulateMode(ResourcePackage resourcePackage)
         {
-            var initParameters = new EditorSimulateModeParameters();
-            var buildResult = EditorSimulateModeHelper.SimulateBuild(EDefaultBuildPipeline.BuiltinBuildPipeline, resourcePackage.PackageName);
-            var packageRoot = buildResult.PackageRootDirectory;
-            var editorFileSystem = FileSystemParameters.CreateDefaultEditorFileSystemParameters(buildResult);
-            initParameters.EditorFileSystemParameters = editorFileSystem;
-            return resourcePackage.InitializeAsync(initParameters);
+            var simulateBuildResult = EditorSimulateModeHelper.SimulateBuild(nameof(EDefaultBuildPipeline.BuiltinBuildPipeline), ConstDefaultPackageName);
+            var createParameters = new EditorSimulateModeParameters();
+            createParameters.EditorFileSystemParameters = FileSystemParameters.CreateDefaultEditorFileSystemParameters(simulateBuildResult);
+            return resourcePackage.InitializeAsync(createParameters);
         }
 
+        /// <summary>
+        /// 初始化YooAsset单机运行模式
+        /// </summary>
+        /// <param name="resourcePackage">资源包</param>
+        /// <returns></returns>
         [UnityEngine.Scripting.Preserve]
         private InitializationOperation InitializeYooAssetOfflinePlayMode(ResourcePackage resourcePackage)
         {
@@ -66,21 +74,30 @@ namespace GameFrameX.Asset.Runtime
             return resourcePackage.InitializeAsync(initParameters);
         }
 
+        /// <summary>
+        /// 初始化YooAsset WebGL运行模式
+        /// </summary>
+        /// <param name="resourcePackage">资源包</param>
+        /// <param name="hostServerURL">主机服务器URL</param>
+        /// <param name="fallbackHostServerURL">备用主机服务器URL</param>
+        /// <returns></returns>
         [UnityEngine.Scripting.Preserve]
         private InitializationOperation InitializeYooAssetWebPlayMode(ResourcePackage resourcePackage, string hostServerURL, string fallbackHostServerURL)
         {
             var initParameters = new WebPlayModeParameters();
-            FileSystemParameters webRemoteFileSystemParams = null;
-            IRemoteServices remoteServices = new RemoteServices(hostServerURL, fallbackHostServerURL);
-            // var webServerFileSystemParams = FileSystemParameters.CreateDefaultWebServerFileSystemParameters();
-            // webServerFileSystemParams.AddParameter(FileSystemParametersDefine.DISABLE_CATALOG_FILE, true);
-
+            FileSystemParameters webFileSystem = null;
 #if UNITY_WEBGL
 #if ENABLE_DOUYIN_MINI_GAME
             // 创建字节小游戏文件系统
             // https: //www.yooasset.com/docs/MiniGame#%E6%8A%96%E9%9F%B3%E5%B0%8F%E6%B8%B8%E6%88%8F
-            string packageRoot = YooAssetSettingsData.Setting.DefaultYooFolderName;
-            webRemoteFileSystemParams = GameFrameX.Asset.YooAsset.Minigame.TikTok.Runtime.ByteGameFileSystemCreater.CreateByteGameFileSystemParameters(packageRoot);
+            if (hostServerURL.IsNullOrWhiteSpace())
+            {
+                webFileSystem = ByteGameFileSystemCreater.CreateByteGameFileSystemParameters();
+            }
+            else
+            {
+                webFileSystem = ByteGameFileSystemCreater.CreateByteGameFileSystemParameters(hostServerURL);
+            }
 #elif ENABLE_WECHAT_MINI_GAME
             //https://www.yooasset.com/docs/MiniGame#%E5%BE%AE%E4%BF%A1%E5%B0%8F%E6%B8%B8%E6%88%8F
             WeChatWASM.WXBase.PreloadConcurrent(10);
@@ -88,35 +105,43 @@ namespace GameFrameX.Asset.Runtime
 #if ENABLE_GAME_FRAME_X_YOO_ASSET_MINI_GAME
             GameEntry.GetComponent<AssetComponent>().gameObject.GetOrAddComponent<WeChatConfigHandler>();
 #endif
-            string packageRoot = $"{WeChatWASM.WXBase.env.USER_DATA_PATH}/__GAME_FILE_CACHE/{YooAssetSettingsData.GetDefaultYooFolderName()}";
+            string packageRoot = $"{WeChatWASM.WXBase.env.USER_DATA_PATH}/__GAME_FILE_CACHE/{YooAssetSettingsData.Setting.DefaultYooFolderName}";
             // 创建微信小游戏文件系统
-
-            webRemoteFileSystemParams = WechatFileSystemCreater.CreateFileSystemParameters(packageRoot, remoteServices);
-            webRemoteFileSystemParams.AddParameter(FileSystemParametersDefine.DISABLE_CATALOG_FILE, true);
+            if (hostServerURL.IsNullOrWhiteSpace())
+            {
+                webFileSystem = WechatFileSystemCreater.CreateWechatFileSystemParameters();
+            }
+            else
+            {
+                webFileSystem = WechatFileSystemCreater.CreateWechatPathFileSystemParameters(hostServerURL);
+            }
 #else
             // 创建默认WebGL文件系统
-            webRemoteFileSystemParams = FileSystemParameters.CreateDefaultWebRemoteFileSystemParameters(remoteServices); //支持跨域下载
+            webFileSystem = FileSystemParameters.CreateDefaultWebFileSystemParameters();
 #endif
 #else
-            webRemoteFileSystemParams = FileSystemParameters.CreateDefaultWebRemoteFileSystemParameters(remoteServices); //支持跨域下载
+            webFileSystem = FileSystemParameters.CreateDefaultWebFileSystemParameters();
 #endif
-            // initParameters.WebServerFileSystemParameters = null;
-            initParameters.WebFileSystemParameters = webRemoteFileSystemParams;
-            // initParameters.AutoUnloadBundleWhenUnused = true;
+            initParameters.WebFileSystemParameters = webFileSystem;
             return resourcePackage.InitializeAsync(initParameters);
         }
 
-        [UnityEngine.Scripting.Preserve]
+        /// <summary>
+        /// 初始化YooAsset热更新运行模式
+        /// </summary>
+        /// <param name="resourcePackage">资源包</param>
+        /// <param name="hostServerURL">主机服务器URL</param>
+        /// <param name="fallbackHostServerURL">备用主机服务器URL</param>
+        /// <returns></returns>
         private InitializationOperation InitializeYooAssetHostPlayMode(ResourcePackage resourcePackage, string hostServerURL, string fallbackHostServerURL)
         {
-            IRemoteServices remoteServices = new RemoteServices(hostServerURL, fallbackHostServerURL);
-            var cacheFileSystem = FileSystemParameters.CreateDefaultCacheFileSystemParameters(remoteServices);
-            var buildinFileSystem = FileSystemParameters.CreateDefaultBuildinFileSystemParameters();
-            var initParameters = new HostPlayModeParameters();
-            initParameters.BuildinFileSystemParameters = buildinFileSystem;
-            initParameters.CacheFileSystemParameters = cacheFileSystem;
-            // initParameters.AutoUnloadBundleWhenUnused = true;
-            return resourcePackage.InitializeAsync(initParameters);
+            var remoteServices = new RemoteServices(hostServerURL, fallbackHostServerURL);
+            var createParameters = new HostPlayModeParameters
+            {
+                BuildinFileSystemParameters = FileSystemParameters.CreateDefaultBuildinFileSystemParameters(),
+                CacheFileSystemParameters = FileSystemParameters.CreateDefaultCacheFileSystemParameters(remoteServices),
+            };
+            return resourcePackage.InitializeAsync(createParameters);
         }
     }
 }
